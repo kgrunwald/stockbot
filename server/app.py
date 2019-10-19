593bef0ea22627a0296922ebe192d7b9ad78ff19
Kyle Grunwald <kgrunwald@gmail.com>
2019-10-19 17:59:43 -0500

    Add jwt handler

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify
+from controllers.auth import Unauthorized
 from controllers import api
+
 
 app = Flask(__name__)
 blueprint = Blueprint('api', __name__, url_prefix='/api')
