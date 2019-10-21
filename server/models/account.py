869483dcc0f6408476ae5ddd861fb77aa4f945c2
Kyle Grunwald <kgrunwald@sparefoot.com>
2019-10-21 16:54:18 -0500

    Implement simple acl

--- server/models/account.py
+++ server/models/account.py
@@ -1,12 +1,15 @@
 from typing import Dict
+from .base import ProtectedModel
 from .db import db, Column
 
 
-class Account(db.Model):
-    id = Column(db.Integer, primary_key=True, description="Unique ID for the account in the database", required=True)
+class Account(ProtectedModel, db.Model):
+    id = Column(db.Integer, primary_key=True,
+                description="Unique ID for the account in the database", required=True)
     api_key = Column(db.String(80), unique=True, nullable=False,
                      description="The Alpaca API Key", required=True)
-    secret_key = Column(db.String(80), nullable=False, description="The Alpaca API Secret Key", required=True)
+    secret_key = Column(db.String(80), nullable=False,
+                        description="The Alpaca API Secret Key", required=True)
 
     def __repr__(self):
         return '<Account %r>' % self.id
