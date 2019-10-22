aa0529c49fb285a8d5eac964278a9f94a1d7a58a
jkorzekwa <james.korzekwa@gmail.com>
2019-10-21 21:04:41 -0500

    add icon and format login page

--- app/src/screens/Login.tsx
+++ app/src/screens/Login.tsx
@@ -1,27 +1,44 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { Image } from "react-native";
 import { useAuth } from "../context/AuthContext";
 
 const Login = () => {
   const auth = useAuth();
   return (
-    <View style={styles.container}>
-      {<Button title="Log in with Auth0" onPress={auth.login} />}
+    <View style={styles.mainContainer}>
+      <View style={styles.titleContainer}>
+        <Image
+          source={require("../../assets/money-bot.png")}
+          style={{ width: 100, height: 100 }}
+        />
+        <Text style={styles.title}>StockBot</Text>
+      </View>
+      <Button color="#EB5424" title="Log in with Auth0" onPress={auth.login} />
+      <View />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-around"
+  },
+  titleContainer: {
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
   },
   title: {
-    fontSize: 20,
+    fontSize: 40,
     textAlign: "center",
-    marginTop: 40
+    marginTop: 20,
+    fontFamily: "Courier-Bold",
+    fontWeight: "400"
   }
 });
 
