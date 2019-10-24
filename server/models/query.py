006d01ea2f9da9fd09c1d8169c77115f49662675
Kyle Grunwald <kgrunwald@gmail.com>
2019-10-23 22:13:45 -0500

    Split query processing into smaller functions

--- server/models/query.py
+++ server/models/query.py
@@ -16,26 +16,41 @@ def acl_check(query: Query):
     if hasattr(query, '__is_admin'):
         return query
 
-    protected_entites = []
-    for desc in query.column_descriptions:
-        entity = desc['entity']
-        if issubclass(entity, ProtectedModel):
-            protected_entites.append(entity)
-        elif entity == User:
-            protected_entites.append(entity)
+    protected_entites = get_protected_entities(query)
 
     if len(protected_entites) > 0:
         jwt = g.jwt
         for entity in protected_entites:
-            if 'scope' in jwt.keys():
-                scope = 'read:{}'.format(entity.__tablename__.lower())
-                if scope in jwt['scope']:
-                    return query
+            scope = 'read:{}'.format(entity.__tablename__.lower())
+            if validate_scope(jwt, scope):
+                continue
 
             if entity == User:
                 query = query.filter(entity.uid == jwt['sub'])
             else:
-                user = admin_query().filter_by(uid=jwt['sub']).first()
-                acl_ids = [p.acl_id for p in user.permissions if p.permission == 'read']
-                query = query.filter(entity.acl_id.in_(acl_ids))
+                acls = get_acls(jwt, 'read')
+                query = query.filter(entity.acl_id.in_(acls))
     return query
+
+
+def validate_scope(jwt: dict, scope: str) -> bool:
+    if 'scope' in jwt.keys():
+        if scope in jwt['scope']:
+            return True
+    return False
+
+
+def get_acls(jwt: dict, permission: str) -> [str]:
+    user = admin_query().filter_by(uid=jwt['sub']).first()
+    return [p.acl_id for p in user.permissions if p.permission == permission]
+
+
+def get_protected_entities(query: Query):
+    entities = []
+    for desc in query.column_descriptions:
+        entity = desc['entity']
+        if issubclass(entity, ProtectedModel):
+            entities.append(entity)
+        elif entity == User:
+            entities.append(entity)
+    return entities
